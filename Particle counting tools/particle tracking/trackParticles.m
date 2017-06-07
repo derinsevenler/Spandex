ff19bc556b1339d54040f69a0bb97b955838f4a5
Derin Sevenler <derin.sevenler@gmail.com>
2017-06-07 16:10:52 -0400

    added some more useful functions for particle tracking

--- Particle counting tools/particle tracking/trackParticles.m
+++ Particle counting tools/particle tracking/trackParticles.m
@@ -7,37 +7,47 @@ function particleList = trackParticles(rc, matches)
 % matches is a cell array of [(n-1)x2] match arrays. 
 % 
 % particleList is a cell array of all the particles detected in 
-% 	all the images. For particle n, particleList(n,1) is an array 
-% 	of which images that particle was in (i.e., [1 2 3]). particleList(n,2)
-% 	is an array of (r,c) particle coordinates in those images. 
+% 	all the images. For particle n, particleList{n,1} is an array 
+% 	of the indices of the images which that particle was in (i.e., 
+%   [1 2 3] - particle was in images 1, 2 and 3). particleList{n,2}
+% 	is an array of the particle's coordinates (r,c) in those images.
 
-% Initialize with particles in the first images
-pList = ones(size(rc{1},1),1); % All 
+% Initialize the particleList with all the particles in the first image
+pList = ones(size(rc{1},1),1);
 particleList = [num2cell(pList) num2cell(rc{1},2)];
 
-% The look-up table is like an updated list of matches
+% The look-up table is like an updated list of matches. It has the
+% same length as rc{n} 
+% Column 1: the index of each particle in this particular frame
+% Column 2: the index (or 'name') of this particle in the particleList
 lut = repmat([1:size(rc{1},1)]',1,2); % dummy initialization lut
 
-% matches gotta be sorted by the first column (previous indices)
+% matches must be sorted by the first column (previous indices)
 oneC = cell(size(matches));
 oneC(:) = {1};
 matches = cellfun(@sortrows, matches,oneC,'UniformOutput', false);
 
 for n = 2:length(rc)
-	 % crazy indexing magic to update lut
+	% crazy indexing magic to update lut:
+    % get the index of particles in the previous frame which have matches
 	[~, lutIdx, ~] = intersect(lut(:,1), matches{n-1}(:,1),'stable');
-	lut = [matches{n-1}(:,2) lut(lutIdx,2)];
-    lastName = size(particleList,1); % The highest particle name
+	% Update the lut:
+    % 1. All the particles at lutIdx have matches here: change the first column to correspond
+    lut = [matches{n-1}(:,2) lut(lutIdx,2)];
+    lut = sortrows(lut, 1);
+
+    % 2. Put (append) any new particles 
+    lastName = size(particleList,1); % The highest particle 'name'
 	unmatchedNames = lastName + (1:(size(rc{n},1)-size(matches{n-1},1))); % names to give to the unmatched particles
     
-    if ~isempty(unmatchedNames) % we need to make particleList longer. If not, every particle is accounted for
+    if ~isempty(unmatchedNames) % we need to make particleList longer. If not, every particle in this frame is accounted for (it matches a previous one)
         unmatched = setdiff(1:size(rc{n},1), lut(:,1)');
         lut = [lut; [unmatched', unmatchedNames']];
-        lut = sortrows(lut, 1); % gotta sort it
+        lut = sortrows(lut, 1); % gotta keep it sorted!
         particleList = [particleList; cell(length(unmatchedNames),2)];
     end
+
     for x = 1:size(lut,1)
 		particleList(lut(x,2),:) = {[particleList{lut(x,2),1} n], [particleList{lut(x,2),2}; rc{n}(lut(x,1),:)]};
     end
-    size(particleList);
 end
